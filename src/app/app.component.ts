ccecbcc6ce6152089577392c13e51136abac75ca
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-10 11:35:48 -0500

    primera configuración!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team-angular31-comfeco';
+	title = 'team-angular31-comfeco';
 }
