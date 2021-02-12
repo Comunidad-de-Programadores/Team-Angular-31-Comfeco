a1b7cdfce41c398067c57275669fe5cb39d7c8a1
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-12 11:51:47 -0500

    se eliminan las carpetas y archivos para que esta rama solo contenga la configuración inicial, a partir de ahora los cambios que entraran a esta rama deben ser a traves de un pull request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { environment } from '../environments/environment';
-
 //Modules
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, ProfileComponent],
+	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
-		AppRoutingModule,
 		FormsModule,
 		AngularFireAuthModule,
 		AngularFireModule.initializeApp(environment.firebase),
