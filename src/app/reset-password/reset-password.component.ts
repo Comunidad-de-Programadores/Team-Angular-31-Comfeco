faf9209d891a487d43d85777de468f7d8350c0bb
kevin <kfariid@gmail.com>
2021-02-13 23:40:54 -0500

    Social login listo para subir

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,63 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-reset-password',
-  templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+	selector: 'app-reset-password',
+	templateUrl: './reset-password.component.html',
+	styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
+	mode = '';
+	actionCode = '';
+	actionCodeChecked = false;
+	newPassword = '';
+	confirmPassword = '';
 
-  mode !: string;
-  actionCode !: string;
-  actionCodeChecked = false;
-  newPassword = '';
-  confirmPassword = '';
+	constructor(
+		private router: Router,
+		private activatedRoute: ActivatedRoute,
+		private auth: AngularFireAuth
+	) {}
 
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private auth: AngularFireAuth) { }
+	ngOnInit(): void {
+		this.activatedRoute.queryParams.subscribe((params: Params) => {
+			// if we didn't receive any parameters,
+			// we can't do anything
+			if (!params) void this.router.navigate(['/login']);
 
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
-      // if we didn't receive any parameters, 
-      // we can't do anything
-      if (!params) this.router.navigate(['/login']);
+			this.mode = String(params['mode']);
+			this.actionCode = String(params['oobCode']);
 
-      this.mode = params['mode'];
-      this.actionCode = params['oobCode'];
+			if (params['mode'] === 'resetPassword') {
+				// Verificar si el código de reseteo es válido
+				this.auth
+					.verifyPasswordResetCode(this.actionCode)
+					.then((email) => {
+						this.actionCodeChecked = true;
+					})
+					.catch((e) => {
+						// Invalid or expired action code
+						alert(e);
+						void this.router.navigate(['/login']);
+					});
+			}
+		});
+	}
 
-      if(params['mode'] === 'resetPassword') {
-        // Verificar si el código de reseteo es válido
-        this.auth.verifyPasswordResetCode(this.actionCode).then(email => {
-          this.actionCodeChecked = true;
-        }).catch(e => {
-          // Invalid or expired action code
-          alert(e);
-          this.router.navigate(['/login']);
-        });
-      }
-     });
-
-  }
-
-  handleResetPassword(){
-    if (this.newPassword != this.confirmPassword) { 
-      alert('New Password and Confirm Password do not match'); 
-      return; 
-    } 
-    // Save the new password.   
-    this.auth.confirmPasswordReset(
-      this.actionCode, 
-      this.newPassword
-    ).then(resp => { 
-      // Password reset has been confirmed and new password updated.    
-      alert('New password has been saved');
-      this.router.navigate(['/login']); }).catch(e => { 
-        // Error occurred during confirmation. 
-        alert(e); 
-      }); 
-  }
+	handleResetPassword() {
+		if (this.newPassword != this.confirmPassword) {
+			alert('New Password and Confirm Password do not match');
+			return;
+		}
+		// Save the new password.
+		this.auth
+			.confirmPasswordReset(this.actionCode, this.newPassword)
+			.then((resp) => {
+				// Password reset has been confirmed and new password updated.
+				alert('New password has been saved');
+				void this.router.navigate(['/login']);
+			})
+			.catch((e) => {
+				// Error occurred during confirmation.
+				alert(e);
+			});
+	}
 }
