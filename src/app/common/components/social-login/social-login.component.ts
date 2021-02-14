b4eda4a8b208b4235b3e5157331db0cf684d06d2
kevin <kfariid@gmail.com>
2021-02-13 23:08:08 -0500

    Corrigiendo errores de ESLint

--- src/app/common/components/social-login/social-login.component.ts
+++ src/app/common/components/social-login/social-login.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-social-login',
 	templateUrl: './social-login.component.html',
 	styleUrls: ['./social-login.component.scss']
 })
-export class SocialLoginComponent {}
+export class SocialLoginComponent {
+	@Input() text = '';
+	@Input() textLink = '';
+}
