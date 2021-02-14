0163034ed4104bfb039909ca47e2d9ad386212a2
Jimy Joaquin Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-13 22:35:47 -0500

    subiendo version inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'team-angular31-comfeco';
+	title = 'team-angular31-comfeco 2021';
 }
