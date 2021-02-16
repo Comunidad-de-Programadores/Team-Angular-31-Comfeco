3854eb32bd8a6622d04a8b5e6dd3690e9992857c
kevin <kfariid@gmail.com>
2021-02-16 13:09:23 -0500

    inicio de componente

--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MoleculesModule } from '@team31/components-molecules/molecules.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
 	declarations: [LoginPageComponent],
-	exports: [LoginPageComponent]
+	exports: [LoginPageComponent],
+	imports: [MoleculesModule]
 })
 export class PageModule {}
