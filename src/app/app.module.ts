0b19e1105d3d9ba0f8ae5dfaa0dda00873704623
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-16 13:53:32 -0500

    buscando error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { environment } from '../environments/environment';
-
 //Modules
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderBannerModule } from '@team31/components-atoms/header-banner.module';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocialFooterComponent } from './common/components/atoms/social-footer/social-footer.component';
+import { SocialLoginComponent } from './common/components/atoms/social-login/social-login.component';
 import { LoginComponent } from './common/components/molecules/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { SocialLoginComponent } from './common/components/atoms/social-login/social-login.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SocialFooterComponent } from './common/components/atoms/social-footer/social-footer.component';
-import { HeaderComponent } from '@team31/components-atoms/header/header.component';
 
 @NgModule({
 	declarations: [
@@ -24,16 +19,15 @@ import { HeaderComponent } from '@team31/components-atoms/header/header.componen
 		LoginComponent,
 		ProfileComponent,
 		SocialLoginComponent,
-		SocialFooterComponent,
-		HeaderComponent
+		SocialFooterComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		FormsModule,
 		AngularFireAuthModule,
 		AngularFireModule.initializeApp(environment.firebase),
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		HeaderBannerModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/common/components/atoms/atoms.module.ts
+++ src/app/common/components/atoms/atoms.module.ts
@@ -1,14 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CardContainerComponent } from './card-container/card-container.component';
+import { HeaderComponent } from './header/header.component';
 import { ModalTerminosComponent } from './modal-terminos/modal-terminos.component';
 import { SocialFooterComponent } from './social-footer/social-footer.component';
 import { SocialLoginComponent } from './social-login/social-login.component';
 import { TerminosCondicionesComponent } from './terminos-condiciones/terminos-condiciones.component';
-import { HeaderComponent } from './header/header.component';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [
 		SocialLoginComponent,
 		CardContainerComponent,
@@ -25,7 +26,6 @@ import { CommonModule } from '@angular/common';
 		TerminosCondicionesComponent,
 		SocialFooterComponent,
 		HeaderComponent
-	],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA]
+	]
 })
 export class AtomModule {}
