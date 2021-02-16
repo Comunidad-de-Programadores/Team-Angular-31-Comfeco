c12601cf18dca373109f703600bac3a1339de7d0
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-16 13:59:23 -0500

    solucion kurts

--- src/app/common/components/molecules/login/login.component.ts
+++ src/app/common/components/molecules/login/login.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 @Component({
 	selector: 'app-login',
-	templateUrl: './login.component.ts',
+	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-	// email = 'kevin@comfeco.com';
-	// password = '123456';
-	// constructor(private auth: AngularFireAuth, private router: Router) {}
-	// async login(): Promise<void> {
-	// 	try {
-	// 		const singIn = await this.auth.signInWithEmailAndPassword(this.email, this.password);
-	// 		if (singIn) {
-	// 			void this.router.navigate(['profile']);
-	// 		}
-	// 	} catch (error) {
-	// 		console.error('Error cl:', error);
-	// 	}
-	// }
-	// resetPassword(): void {
-	// 	void this.auth
-	// 		.sendPasswordResetEmail(this.email)
-	// 		.then(
-	// 			() => alert('A password reset link has been sent to your emailaddress'),
-	// 			(rejectionReason) => alert(rejectionReason)
-	// 		)
-	// 		.catch(() => alert('An error occurred while attempting to reset your password'));
-	// }
+	email = 'kevin@comfeco.com';
+	password = '123456';
+	constructor(private auth: AngularFireAuth, private router: Router) {}
+	async login(): Promise<void> {
+		try {
+			const singIn = await this.auth.signInWithEmailAndPassword(this.email, this.password);
+			if (singIn) {
+				void this.router.navigate(['profile']);
+			}
+		} catch (error) {
+			console.error('Error cl:', error);
+		}
+	}
+	resetPassword(): void {
+		void this.auth
+			.sendPasswordResetEmail(this.email)
+			.then(
+				() => alert('A password reset link has been sent to your emailaddress'),
+				(rejectionReason) => alert(rejectionReason)
+			)
+			.catch(() => alert('An error occurred while attempting to reset your password'));
+	}
 }
--- src/app/common/components/molecules/molecules.module.ts
+++ src/app/common/components/molecules/molecules.module.ts
@@ -1,14 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AtomModule } from '../atoms/atoms.module';
-import { LoginComponent } from './login/login.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { AtomModule } from '../atoms/atoms.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [LoginComponent],
 	imports: [
 		AtomModule,
