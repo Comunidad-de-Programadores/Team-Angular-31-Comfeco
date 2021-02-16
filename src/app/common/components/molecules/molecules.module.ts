352c7556cfe922572d6e246e87580e911c90c32c
dstefanow15987 <dstefano_159753@hotmail.com>
2021-02-16 13:38:09 -0500

    arreglar1

--- src/app/common/components/molecules/molecules.module.ts
+++ src/app/common/components/molecules/molecules.module.ts
@@ -1,11 +1,23 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AtomModule } from '../atoms/atoms.module';
 import { LoginComponent } from './login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [LoginComponent],
-	imports: [AtomModule],
-	exports: [LoginComponent],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA]
+	imports: [
+		AtomModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatIconModule,
+		MatButtonModule
+	],
+	exports: [LoginComponent]
 })
 export class MoleculesModule {}
--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -1,9 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MoleculesModule } from '@team31/components-molecules/molecules.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [LoginPageComponent],
+	imports: [MoleculesModule],
 	exports: [LoginPageComponent]
 })
 export class PageModule {}
