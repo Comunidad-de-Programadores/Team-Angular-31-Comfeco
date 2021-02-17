f982da657f726e1709f37be96472d1b6a37e67cb
whisgelito <whisgeljesus10@gmail.com>
2021-02-17 01:57:22 -0400

    Registo

--- src/app/common/components/molecules/molecules.module.ts
+++ src/app/common/components/molecules/molecules.module.ts
@@ -1,9 +1,31 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AtomModule } from '../atoms/atoms.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+
 @NgModule({
-	declarations: [LoginComponent],
-	imports: [AtomModule],
-	exports: [LoginComponent]
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	declarations: [
+			LoginComponent,
+		 	RegisterComponent
+		],
+	imports: [
+		       CommonModule,
+		       AtomModule,
+		       MatFormFieldModule,
+		       MatIconModule,
+		       MatInputModule,
+		       MatButtonModule,
+			   MatCheckboxModule,
+			   ReactiveFormsModule
+			 ],
+	exports: [LoginComponent, RegisterComponent]
 })
 export class MoleculesModule {}
---
+++ src/app/common/components/molecules/register/register.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { IRegister } from '../../../models/register';
+import { ModalServiceService } from '../../../services/modal-service.service';
+import { MatDialog } from '@angular/material/dialog';
+import { IDataDialog } from '../../../models/data-dialog';
+import { VariableStatic } from '../../../static/variable-static';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  data: IDataDialog = {
+    titleModal: 'Vacio',
+    contentModal: 'Lleno'
+  };
+  public registerForm = new FormGroup({
+    userName: new FormControl,
+    email: new FormControl,
+    pass: new FormControl,
+    confirmPass: new FormControl,
+  });
+ 
+  declarations: IRegister = {};
+  constructor(private formBuilder: FormBuilder, private modalService:ModalServiceService, public dialog: MatDialog ) { 
+ 
+    this.declarations = {
+      
+       button: 'Registrar',
+       
+        enlace: 'Volver al login',
+    }
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      userName: [ '', [Validators.required, 
+                        Validators.minLength(4),
+                        Validators.maxLength(10)]],
+      email: [
+         '', [Validators.required,
+                     Validators.email]
+                    ],
+
+      pass: [ '', Validators.required, 
+                  Validators.minLength(4),
+                  Validators.maxLength(10)],
+      confirmPass: [ '', Validators.required, 
+                        Validators.minLength(4),
+                        Validators.maxLength(10)],
+    })
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+  clickModalTerms(){
+  this.modalService.open(VariableStatic.REGISTRATION_TERMS_CONDITIONS);
+  }
+
+}
