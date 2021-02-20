f8cf416e551a03d7431eeacf4f5cef6d363a673f
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-20 04:13:16 -0500

    avanzando el home

--- src/app/common/components/atoms/header-banner.module.ts
+++ src/app/common/components/atoms/header-banner.module.ts
@@ -1,11 +1,19 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { AvatarComponent } from './avatar/avatar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { SocialFooterComponent } from './social-footer/social-footer.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [HeaderComponent, FooterComponent, SocialFooterComponent],
+	declarations: [
+		HeaderComponent,
+		FooterComponent,
+		SocialFooterComponent,
+		NavMenuComponent,
+		AvatarComponent
+	],
 	exports: [HeaderComponent, FooterComponent]
 })
 export class HeaderBannerModule {}
