a8f7125e65c6f5ed636a8eaad7af91eea72357b5
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-02-27 21:32:55 -0500

    ejemplo carousel

--- src/app/common/components/molecules/carousel/carousel.component.ts
+++ src/app/common/components/molecules/carousel/carousel.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import Glide from '@glidejs/glide';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
+
 @Component({
 	selector: 'app-carousel',
 	templateUrl: './carousel.component.html',
 	styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
-	glide: any;
+export class CarouselComponent implements AfterViewInit {
+	@ViewChild('myCarousel') myCarousel: NguCarousel<any>;
 
-	ngOnInit(): void {
-		this.glide = new Glide('.glide', {
-			type: 'carousel'
-		}).mount();
+	name = 'Angular';
+	slideNo = 0;
+	withAnim = true;
+	resetAnim = true;
+	carouselItems = [1, 2, 3, 4, 5, 6, 7, 8];
+	carouselConfig: NguCarouselConfig = {
+		grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+		slide: 3,
+		interval: { timing: 4000, initialDelay: 1000 },
+		loop: false,
+		touch: true,
+		velocity: 0.2
+	};
+	constructor(private cdr: ChangeDetectorRef) {}
+
+	ngAfterViewInit() {
+		this.cdr.detectChanges();
+	}
+
+	reset() {
+		this.myCarousel.reset(!this.resetAnim);
+	}
+
+	moveTo(slide) {
+		this.myCarousel.moveTo(slide, !this.withAnim);
 	}
 }
--- src/app/common/components/molecules/molecules.module.ts
+++ src/app/common/components/molecules/molecules.module.ts
@@ -1,11 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NguCarouselModule } from '@ngu/carousel';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [],
-	imports: [],
+	declarations: [CarouselComponent],
+	imports: [NguCarouselModule],
 
-	exports: [],
+	exports: [CarouselComponent],
 	providers: []
 })
 export class MoleculesModule {}
