e37d2e3c145a0be8ce952b949df153f6ddc24d2a
kevin <kfariid@gmail.com>
2021-03-01 23:32:44 -0500

    avance de pagina principal, falta responsive y detalles esteticos

--- src/app/common/components/atoms/atoms.module.ts
+++ src/app/common/components/atoms/atoms.module.ts
@@ -3,13 +3,19 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 import { CardContainerComponent } from './card-container/card-container.component';
+import { CounterComponent } from './counter/counter.component';
 import { ModalTerminosComponent } from './modal-terminos/modal-terminos.component';
 import { SocialLoginComponent } from './social-login/social-login.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [SocialLoginComponent, CardContainerComponent, ModalTerminosComponent],
+	declarations: [
+		SocialLoginComponent,
+		CardContainerComponent,
+		ModalTerminosComponent,
+		CounterComponent
+	],
 	imports: [MatDialogModule, CommonModule, RouterModule],
-	exports: [SocialLoginComponent, CardContainerComponent, ModalTerminosComponent]
+	exports: [SocialLoginComponent, CardContainerComponent, ModalTerminosComponent, CounterComponent]
 })
 export class AtomModule {}
--- src/app/pages/principal/principal.module.ts
+++ src/app/pages/principal/principal.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { AtomModule } from '@team31/components-atoms/atoms.module';
+import { MoleculesModule } from '@team31/components-molecules/molecules.module';
 import { PrincipalRoutingModule } from './principal-routing.module';
+import { PrincipalComponent } from './principal.component';
 
 @NgModule({
-	imports: [PrincipalRoutingModule],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	declarations: [PrincipalComponent],
+	imports: [PrincipalRoutingModule, MoleculesModule, AtomModule],
 	exports: [],
 	providers: []
 })
