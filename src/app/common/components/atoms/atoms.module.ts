32357feb28d62faaba9de2c77bb0124023c08186
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-03-03 20:44:23 -0500

    se agrega el menu al perfil

--- src/app/common/components/atoms/atoms.module.ts
+++ src/app/common/components/atoms/atoms.module.ts
@@ -1,15 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 import { CardContainerComponent } from './card-container/card-container.component';
+import { CounterComponent } from './counter/counter.component';
 import { ModalTerminosComponent } from './modal-terminos/modal-terminos.component';
 import { SocialLoginComponent } from './social-login/social-login.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { CounterComponent } from './counter/counter.component';
 import { TeamLeaderComponent } from './team-leader/team-leader.component';
-
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [
