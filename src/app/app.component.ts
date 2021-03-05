3847261ab7378fcc98dc2cdec54a3068284a415c
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-03-05 15:24:29 -0500

    finalizando componente menu flotante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'team-angular31-comfeco';
+	demo() {
+		console.log('*****');
+	}
 }
--- src/app/common/components/molecules/molecules.module.ts
+++ src/app/common/components/molecules/molecules.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { NguCarouselModule } from '@ngu/carousel';
 import { AtomModule } from '@team31/components-atoms/atoms.module';
 import { CarouselComponent } from './carousel-leaders/carousel.component';
+import { MenuCommunityComponent } from './menu-community/menu-community.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [CarouselComponent],
-	imports: [NguCarouselModule, CommonModule, AtomModule],
+	declarations: [CarouselComponent, MenuCommunityComponent],
+	imports: [NguCarouselModule, CommonModule, AtomModule, MatIconModule],
 
-	exports: [CarouselComponent],
+	exports: [CarouselComponent, MenuCommunityComponent],
 	providers: []
 })
 export class MoleculesModule {}
