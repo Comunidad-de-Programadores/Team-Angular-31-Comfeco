a4aa6671c5d8276a3cb8ed0151223f094fc9d29e
kevin <kfariid@gmail.com>
2021-03-16 21:58:44 -0500

    integracion finalizada

--- src/app/pages/authentication/guards/resetpassword.guard.ts
+++ src/app/pages/authentication/guards/resetpassword.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { AuthService } from '../../../common/services/auth.service';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class ResetpasswordGuard implements CanActivate {
 
 	async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
 		try {
+			if (environment.production) return true;
 			if (!route.queryParams) return false;
 			if (route.queryParams['mode'] === 'resetPassword') {
 				await this.auth.verifyPasswordresetCode(route.queryParams['oobCode']);
