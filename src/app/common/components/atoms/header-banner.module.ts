5fcca26a9c36eb9985f6003bc36ec87e22b1b840
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-03-20 02:01:25 -0500

    cambios

--- src/app/common/components/atoms/header-banner.module.ts
+++ src/app/common/components/atoms/header-banner.module.ts
@@ -3,13 +3,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { AuthService } from '@team31/services/auth.service';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { NavMenuMovilComponent } from './nav-menu-movil/nav-menu-movil.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { SocialFooterComponent } from './social-footer/social-footer.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { NavMenuMovilComponent } from './nav-menu-movil/nav-menu-movil.component';
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	declarations: [
@@ -20,7 +21,7 @@ import { NavMenuMovilComponent } from './nav-menu-movil/nav-menu-movil.component
 		UserProfileComponent,
 		NavMenuMovilComponent
 	],
-	imports: [MatBadgeModule, MatIconModule, CommonModule, MatMenuModule],
+	imports: [MatBadgeModule, MatIconModule, CommonModule, MatMenuModule, MatTooltipModule],
 	providers: [AuthService],
 	exports: [HeaderComponent, FooterComponent]
 })
--- src/app/pages/authentication/login/login-page.component.ts
+++ src/app/pages/authentication/login/login-page.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@team31/services/auth.service';
 import { HeaderService } from '@team31/services/header.service';
 import { MessageService } from '@team31/services/message.service';
 import { ProfileService } from '@team31/services/profile.service';
 import { UserdataService } from '@team31/services/userdata.service';
-import { Subscription } from 'rxjs';
 @Component({
 	selector: 'app-login-page',
 	templateUrl: './login-page.component.html',
 	styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnDestroy {
+export class LoginPageComponent implements OnInit {
 	email = 'kevin@comfeco.com';
 	password = '123456';
 	hidePassword = true;
-	dataSubscription: Subscription | undefined;
 	isLoading = false;
 	constructor(
 		private authFirebaseService: AuthService,
@@ -25,9 +23,8 @@ export class LoginPageComponent implements OnDestroy {
 		private headerService: HeaderService,
 		private profileService: ProfileService
 	) {}
-
-	ngOnDestroy(): void {
-		this.dataSubscription?.unsubscribe();
+	ngOnInit(): void {
+		this.authFirebaseService.logout();
 	}
 
 	async login(): Promise<void> {
@@ -52,7 +49,7 @@ export class LoginPageComponent implements OnDestroy {
 			}
 		} catch (error) {
 			this.isLoading = false;
-			this._messageService.openError(error, 'end', 'top');
+			this._messageService.openError('Ups!, ocurrio un erro, intenta nuevamente.', 'end', 'top');
 			console.error('Error cl:', error);
 		}
 	}
